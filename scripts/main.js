a3c8a0934bdf568fc285c096ff50192d0f382f45
Artem Bobylev <artematrr@gmail.com>
2024-07-25 20:51:16 +0500

    move 5 files, create 13 files and update 12 files

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,109 @@
 new Swiper('.projects__swiper', {
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+	slidesPerView: 1,
+	spaceBetween: 50,
+	effect: 'fade',
+	keyboard: {
+		enabled: true,
+	},
+	navigation: {
+		prevEl: '.projects__swiper-prev',
+		nextEl: '.projects__swiper-next',
+	},
+})
+
+new Swiper('.animals__tabs', {
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	mousewheel: true,
+	freeMode: true,
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+})
+
+new Swiper('.animals__swiper', {
+	spaceBetween: 0,
+	breakpoints: {
+		320: {
+			centeredSlides: true,
+			slidesPerView: 1,
+		},
+		540: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+		1240: {
+			slidesPerView: 4,
+		},
+		1920: {
+			slidesPerView: 5,
+		},
+		2560: {
+			slidesPerView: 5,
+			centeredSlides: true,
+		},
+	},
+})
+
+new Swiper('.partners__swiper', {
+	grid: {
+		fill: 'row',
+		rows: 2,
+	},
+	spaceBetween: 15,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			centeredSlides: true,
+			grid: {
+				fill: 'row',
+				rows: 1,
+			},
+		},
+		540: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		1024: {
+			slidesPerView: 4,
+		},
+		1440: {
+			slidesPerView: 5,
+		},
+	},
+	navigation: {
+		prevEl: '.partners__swiper-prev',
+		nextEl: '.partners__swiper-next',
+	},
+})
 
 
-});
+new Swiper('.news__swiper', {
+	spaceBetween: 0,
+	breakpoints: {
+		320: {
+			centeredSlides: true,
+			slidesPerView: 1,
+		},
+		540: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+		1240: {
+			slidesPerView: 4,
+		},
+		1920: {
+			slidesPerView: 5,
+		},
+		2560: {
+			slidesPerView: 5,
+			centeredSlides: true,
+		},
+	},
+})
