499e814ba34ef606052d6bf8d581f83a7c26fd59
Artem Bobylev <artematrr@gmail.com>
2024-08-22 18:47:58 +0400

    update 18 files and delete 1 file

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-new Swiper('.projects__swiper', {
+const projectSwiper = new Swiper('.projects__swiper', {
 	slidesPerView: 1,
 	spaceBetween: 50,
 	effect: 'fade',
@@ -9,6 +9,41 @@ new Swiper('.projects__swiper', {
 		prevEl: '.projects__swiper-prev',
 		nextEl: '.projects__swiper-next',
 	},
+})
+
+$('#project-1').click(projectSwiper, function () {
+	projectSwiper.slideTo(0)
+})
+$('#project-2').click(projectSwiper, function () {
+	projectSwiper.slideTo(1)
+})
+$('#project-3').click(projectSwiper, function () {
+	projectSwiper.slideTo(2)
+})
+$('#project-4').click(projectSwiper, function () {
+	projectSwiper.slideTo(3)
+})
+$('#project-5').click(projectSwiper, function () {
+	projectSwiper.slideTo(4)
+})
+$('#project-6').click(projectSwiper, function () {
+	projectSwiper.slideTo(5)
+})
+$('#project-7').click(projectSwiper, function () {
+	projectSwiper.slideTo(6)
+})
+
+$(document).ready(function () {
+	$('.projects__item-inner').on('click touchend', function () {
+		$('.projects__swiper-wrapper').removeClass('is-hidden')
+		$('.projects__wrapper').addClass('is-hidden')
+	})
+})
+$(document).ready(function () {
+	$('.projects__card-close-button').on('click touchend', function () {
+		$('.projects__swiper-wrapper').addClass('is-hidden')
+		$('.projects__wrapper').removeClass('is-hidden')
+	})
 })
 
 new Swiper('.animals__tabs', {
@@ -92,6 +127,25 @@ new Swiper('.news__swiper', {
 		},
 		1240: {
 			slidesPerView: 4,
-		},		
+		},
 	},
 })
+
+new Swiper('.help-now__tabs', {
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	mousewheel: true,
+	freeMode: true,
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+})
+
+$('.js-aside-menu-open').on('click touchend', function () {
+	$('.aside-menu').addClass('is-open')
+})
+$('.js-aside-menu-close').on('click touchend', function () {
+	$('.aside-menu').removeClass('is-open')
+})
+
+// При нажатии на
